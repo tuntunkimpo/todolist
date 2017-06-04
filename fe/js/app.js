2d0d5b76eb665e6f85ad3f24313aa21ec91567ed
bill <tuntunkimpo@naver.com>
2017-06-04 15:09:11 +0900

    ajax활용해서 GET방식으로 ./api/todos url로 리스트를 요청하고 json 방식으로 값을 받아와서 .todo-list 클래스에 내요을 출력 ,오류 발생시 에러 메시지 출력

--- fe/js/app.js
+++ fe/js/app.js
@@ -1,6 +1,44 @@
 (function (window) {
 	'use strict';
+	// html페이지가 화면에 뿌려지고 나서 ready 안에 이벤트들이 동작준비
+	$(document).ready(function() {
+		main();
+	});
 
-	// Your starting point. Enjoy the ride!
+	// 이벤트들 모음
+	function main() {
+		// todo 리스트 Ajax로 불러와서 화면에 뿌려주기
+		$.ajax({
+			url: './api/todos',
+			method: 'GET',
+			dataType: 'json',
+			success: function(data) {
+				var items = [];
+				var itemLeftCount = 0;
+				for(var i = 0; i < data.length; i++) {
+					var className = '';
+					var inputChecked = '';
+					
+					if(data[i].completed == 1) {
+						className = ' class="completed"';
+						inputChecked = ' checked';
+					} else {
+						itemLeftCount++;
+					}
+					items.push("<li" + className + " data-id=" + data[i].id + ">" + "<div class='view'><input class='toggle' type='checkbox'" + inputChecked + ">\
+								<label>" + data[i].todo + "</label><button class='destroy'></button></div></li>");
+				}
+				$('.todo-list').html(items);
+				$('.todo-count > strong').text(itemLeftCount);
+			},
+			error: function() {
+				$('.new-todo').attr('disabled', 'disabled');
+				alert('오류가 발생하였습니다. 잠시 후 다시 시도해 주세요.');
+				$('.main').html('<ul class="todo-list"><li><div class="view"><label>Loading Failed</label></div></li></ul>');
+			}
+		})
+	}
 
+
+	}  
 })(window);
